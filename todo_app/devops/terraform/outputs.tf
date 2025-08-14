17830636931b36128f134831cdfddeffcf8c1408
kishore <kishore@gmail.com>
2025-08-14 15:45:56 +0530

    devops,jenkinsfile,k8s

--- todo_app/devops/terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "ecr_url"         { value = aws_ecr_repository.app.repository_url }
-output "cluster_name"    { value = module.eks.cluster_name }
-output "cluster_endpoint"{ value = module.eks.cluster_endpoint }
--- todo_app/devops/terraform/variables.tf
+++
@@ -1,2 +0,0 @@
-variable "name"       { type = string, default = "Django_todo_app" }
-variable "aws_region" { type = string, default = "us-east-1" }
